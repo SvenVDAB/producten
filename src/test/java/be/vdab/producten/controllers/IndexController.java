3d3fd151153a71fbde0c256a5df46045086f4d04
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-03-20 16:06:13 +0100

    3

---
+++ src/test/java/be/vdab/producten/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package be.vdab.producten.controllers;
+
+import be.vdab.producten.domain.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class IndexController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/")
+    List<Product> findAll() {
+        logger.info("GET request naar alle producten");
+        return List.of(new Product(1, "appel"),
+                new Product(2, "peer"),
+                new Product(3, "noot"));
+    }
+}
---
+++ src/test/java/be/vdab/producten/domain/Product.java
@@ -0,0 +1,12 @@
+package be.vdab.producten.domain;
+
+public class Product {
+    private long id;
+    private String naam;
+
+    public Product(long id, String naam) {
+        this.id = id;
+        this.naam = naam;
+    }
+
+}
