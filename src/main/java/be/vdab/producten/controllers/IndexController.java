09fc45ce85c424348f02aa4a179ea69dc40ddb59
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-04-06 17:42:42 +0200

    9_8_4

--- src/main/java/be/vdab/producten/controllers/IndexController.java
+++ src/main/java/be/vdab/producten/controllers/IndexController.java
@@ -6,7 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Random;
 
 
 @RestController
@@ -16,6 +18,11 @@ class IndexController {
     @GetMapping("/")
     List<Product> findAll() {
         logger.info("GET request naar alle producten");
+        var random = new Random();
+        var totaal = BigDecimal.ZERO;
+        for (var teller = 0; teller < 7_000_000; teller++) {
+            totaal = totaal.add(BigDecimal.valueOf(random.nextDouble()));
+        }
         return List.of(new Product(1, "appel"),
                 new Product(2, "peer"),
                 new Product(3, "noot"));
